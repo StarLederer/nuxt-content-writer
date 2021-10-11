28b31bc7a431eb8e44c1f5292268ae85972e3c02
Herman <germans.lederers@gmail.com>
2021-10-11 14:51:50 +0200

    Add an empty SelectFile placeholder for production

--- src/module.ts
+++ src/module.ts
@@ -8,20 +8,27 @@ const CONFIG_KEY = 'nuxtEditor'
 const nuxtModule: Module<ModuleOptions> = function () {
   const options: NuxtOptions = this.nuxt.options
 
-  if (!options.dev) {
-    return
+  if (options.dev) {
+    // Server middleware
+    this.addServerMiddleware({
+      path: '/_editor',
+      handler: getMiddleware(options)
+    })
+
+    // Dev editor component
+    this.addTemplate({
+      fileName: 'editor/SelectFile.vue',
+      src: require.resolve('./templates/SelectFile.vue')
+    })
+  } else {
+    // Production editor compoent (empty)
+    this.addTemplate({
+      fileName: 'editor/SelectFile.vue',
+      src: require.resolve('./templates/SelectFile.prod.vue')
+    })
   }
 
-  this.addServerMiddleware({
-    path: '/_editor',
-    handler: getMiddleware(options)
-  })
-
-  this.addTemplate({
-    fileName: 'editor/SelectFile.vue',
-    src: require.resolve('./templates/SelectFile.vue')
-  })
-
+  // Component loader plugin
   this.addPlugin({
     fileName: 'editor/test.js',
     src: require.resolve('./templates/plugin')
